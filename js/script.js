07c7cc930aa5d014ac6620bf49e87fc6740c649e
UrosZikic <uroszikic1512988@gmail.com>
2024-01-30 12:09:03 +0100

    menu fix

--- js/script.js
+++ js/script.js
@@ -27,38 +27,48 @@ const menu_program_link = document.querySelectorAll(".menu_program_link");
 window.addEventListener("resize", deal_with_dropdown);
 window.addEventListener("load", deal_with_dropdown);
 
-function deal_with_dropdown() {
-  if (body.offsetWidth <= 1155) {
-    let isMenuVisible = false;
+function add_styles() {
+  menu_program.classList.add("display_toggle");
+  navlist.classList.add("navlist_extend_height");
+  cenovnik.classList.add("extend_padding");
+}
 
+function remove_styles() {
+  menu_program.classList.remove("display_toggle");
+  navlist.classList.remove("navlist_extend_height");
+  cenovnik.classList.remove("extend_padding");
+}
+
+function deal_with_dropdown() {
+  let isMenuVisible = false;
+  if (body.offsetWidth <= 1155) {
     menu_trigger.addEventListener("mouseover", () => {
-      menu_program.classList.toggle("display_toggle");
-      navlist.classList.toggle("navlist_extend_height");
-      cenovnik.classList.toggle("extend_padding");
+      add_styles();
       isMenuVisible = true;
     });
 
     menu_trigger.addEventListener("mouseout", () => {
       if (isMenuVisible) {
-        menu_program.classList.remove("display_toggle");
-        navlist.classList.remove("navlist_extend_height");
-        cenovnik.classList.remove("extend_padding");
+        remove_styles();
         isMenuVisible = false;
       }
     });
-  } else {
-    let isMenuVisible = false;
+  } else if (body.offsetWidth > 1155) {
+    navlist.classList.remove("navlist_extend_height");
+    cenovnik.classList.remove("extend_padding");
 
     menu_trigger.addEventListener("mouseover", () => {
       if (document.body.offsetWidth > 1155 && !isMenuVisible) {
         menu_program.classList.add("display_toggle");
+        navlist.classList.remove("navlist_extend_height");
+        cenovnik.classList.remove("extend_padding");
         isMenuVisible = true;
       }
     });
 
     menu_trigger.addEventListener("mouseout", () => {
-      if (isMenuVisible) {
-        menu_program.classList.remove("display_toggle");
+      if (isMenuVisible && document.body.offsetWidth > 1155) {
+        remove_styles();
         isMenuVisible = false;
       }
     });

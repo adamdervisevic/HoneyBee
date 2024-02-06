296f0514d8948c3ccfccfba362964972976756f6
UrosZikic <uroszikic1512988@gmail.com>
2024-02-06 18:53:06 +0100

    new section commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,3 +80,40 @@ function deal_with_dropdown() {
     });
   }
 }
+
+// reason why section
+const reasons_why_container = document.querySelector(".reasons_why");
+const reasons_why_btn = document.querySelectorAll(".section_why_hive button");
+const reason_why = document.querySelector(".reason_why");
+
+// collect value of pixel
+const rect = reasons_why_container.getBoundingClientRect();
+// rect.top = distance form the top; window.pageYOffset = total distance
+const distanceFromTop = rect.top + window.pageYOffset;
+
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset * 4 >= distanceFromTop) {
+    reasons_why_container.classList.remove("reasons_why_disappear");
+    window.removeEventListener("scroll", arguments.callee);
+  }
+});
+
+reasons_why_btn.forEach(
+  (btn, index) =>
+    (btn.onclick = () => {
+      reason_why.classList.add("reason_why_disappear");
+      setTimeout(() => {
+        if (index === 0) {
+          reason_why.innerHTML =
+            "Online časovi nude fleksibilnost u rasporedu, omogućavajući studentima da usklade svoje studije sa drugim obavezama kao što su posao ili porodične obaveze. Ova fleksibilnost omogućava učenicima da kreiraju personalizovane rutine učenja koje odgovaraju njihovim individualnim potrebama.";
+        } else if (index === 1) {
+          reason_why.innerHTML =
+            "Pristupačnost:</strong> Online časovi eliminišu geografske barijere, omogućavajući studentima iz različitih pozadina i lokacija pristup kvalitetnom obrazovanju. Bez obzira da li se nalaze u ruralnim područjima ili u prometnim urbanim centrima, pojedinci mogu da se bave obrazovnim prilikama bez ograničenja fizičke udaljenosti.";
+        } else {
+          reason_why.innerHTML =
+            "Online časovi pružaju pristup raznovrsnim resursima, uključujući multimedijalne materijale, interaktivne simulacije i virtualne laboratorije. Ova raznovrsnost unapređuje iskustvo učenja nudeći više puteva za istraživanje i razumevanje, prilagođavajući se različitim stilovima učenja.";
+        }
+        reason_why.classList.remove("reason_why_disappear");
+      }, 500);
+    })
+);
--- index.php
+++ index.php
@@ -14,10 +14,10 @@ include "components/header.php";
             <a href="#" class="ctaa"><i class="ri-play-fill"></i>Watch</a>
         </div>
 
-        <div class="hive">
-            <div class="hexagon hex-s"></div>
-            <div class="hexagon hex-m"></div>
-            <div class="hexagon hex-b"></div>
+        <div class="hive header_hive">
+            <div class="hexagon header_hexagon hex-s"></div>
+            <div class="hexagon header_hexagon hex-m"></div>
+            <div class="hexagon header_hexagon hex-b"></div>
         </div>
     </section>
 
@@ -26,6 +26,28 @@ include "components/header.php";
         <a href="#"><i class="ri-youtube-line"></i></a>
         <a href="#"><i class="ri-facebook-line"></i></a>
     </div>
+
+    <section class="reasons_why reasons_why_disappear">
+        <h2>Zasto online nastava</h2>
+        <div class="section_why_hive">
+            <button>
+                <div class="hexagon hex-why">
+                    <p>Fleksibilnost</p>
+                </div>
+            </button>
+            <button>
+                <div class="hexagon hex-why">
+                    <p>Pristupacnost</p>
+                </div>
+            </button>
+            <button>
+                <div class="hexagon hex-why">
+                    <p>Resursi</p>
+                </div>
+            </button>
+        </div>
+        <p class="reason_why"></p>
+    </section>
 </main>
 <?php
 include "components/footer.php";
